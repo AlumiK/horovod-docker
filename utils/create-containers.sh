48e3a8e19d391502dfad2d254edcda0d2edc31d1
Zhong Zhenyu <nczzy1997@gmail.com>
2020-11-27 10:58:20 +0800

    Remove personal info

--- utils/create-containers.sh
+++ utils/create-containers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for i in {1..16}
-    do docker run -itd --network=horovod --runtime=nvidia --shm-size=1g --name node-$i --privileged --ip 172.21.0.$((1 + $i)) alumik/horovod:cuda-mpi
+    do docker run -itd --network=horovod --runtime=nvidia --shm-size=1g --name node-$i --privileged --ip 172.21.0.$((1 + $i)) horovod:cuda-mpi
 done
