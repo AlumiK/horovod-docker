f316186d3910c9116046663a49d9e310319a3045
Zhong Zhenyu <nczzy1997@gmail.com>
2020-12-16 03:56:24 -0500

    Bind port 11022

--- utils/cpu_dev/create.sh
+++ utils/cpu_dev/create.sh
@@ -2,6 +2,6 @@
 
 HOROVOD_DOCKER_PATH=$1
 
-docker run -itd --shm-size=1g --name node-dev --privileged \
+docker run -itd --shm-size=1g --name node-dev --privileged -p 11022:11022 \
     -v $HOROVOD_DOCKER_PATH:/root/horovod-docker \
     horovod-dev:cpu
