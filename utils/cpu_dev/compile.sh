597d8042c1b9dfb62f1e85e8fefbd84c970f39fa
Zhong Zhenyu <nczzy1997@gmail.com>
2021-01-15 17:12:36 +0800

    Fix: Failed to determine if Gloo support has been built

--- utils/cpu_dev/compile.sh
+++ utils/cpu_dev/compile.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cd ~/horovod
-rm -rf build/ dist/
+rm -rf build/ dist/ horovod.egg-info/
 
-HOROVOD_WITH_TENSORFLOW=1 HOROVOD_WITH_PYTORCH=1 HOROVOD_WITH_MXNET=1 \
+HOROVOD_WITH_TENSORFLOW=1 MAKEFLAGS="-j8" \
     pip install --no-cache-dir -e .
+
+cp build/lib.linux-x86_64-3.8/horovod/metadata.json horovod/metadata.json
