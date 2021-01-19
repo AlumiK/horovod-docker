f77d9a5582b492d283c5c29754e906aca217e015
Zhong Zhenyu <nczzy1997@gmail.com>
2021-01-19 00:05:57 -0500

    Increase complie threads to 32

--- utils/cpu_dev/compile.sh
+++ utils/cpu_dev/compile.sh
@@ -3,7 +3,7 @@
 cd ~/horovod
 rm -rf build/ dist/ horovod.egg-info/
 
-HOROVOD_WITH_TENSORFLOW=1 MAKEFLAGS="-j8" \
+HOROVOD_WITH_TENSORFLOW=1 MAKEFLAGS="-j32" \
     pip install --no-cache-dir -e .
 
 cp build/lib.linux-x86_64-3.8/horovod/metadata.json horovod/metadata.json
